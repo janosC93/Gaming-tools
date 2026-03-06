f55fb07674e0ff05f1014dfefa8ffdc1dc91011c
janosC93 <christopher.janos@hotmail.com>
2026-03-06 10:45:24 +0100

    Fix overlay_link URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const STORE = {
 
 fps_link: "https://janosphere30.gumroad.com/l/thhmex",
-overlay_link: "https://gumroad.com/",
+overlay_link:"https://janosphere30.gumroad.com/l/tniffr
 assets_link: "https://gumroad.com/"
 
 }
