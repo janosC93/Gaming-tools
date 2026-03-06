219d060da0d5e23c5cd220dfce65f42274e53701
janosC93 <christopher.janos@hotmail.com>
2026-03-06 11:02:02 +0100

    Update assets_link to new Gumroad URL

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const STORE = {
 
 fps_link: "https://janosphere30.gumroad.com/l/thhmex",
 overlay_link:"https://janosphere30.gumroad.com/l/tniffr
-assets_link: "https://gumroad.com/"
+assets_link: "https://janosphere30.gumroad.com/l/oeywu
 
 }
